fab61d70034a296c0a71f3648417f28f5370e073
MiguelSerralde <85590150+MiguelSerralde@users.noreply.github.com>
2025-09-13 13:53:36 -0700

    showError

---
+++ audiodb/src/component/Albums/albums.js
@@ -0,0 +1,13 @@
+import "../../css/style.css"
+import React from 'react'
+
+const Albums = () => {
+  return (
+    <div className='albums_container'>
+        <h3>Albums</h3>
+        <section></section>
+    </div>
+  )
+}
+
+export default Albums
--- audiodb/src/component/Library/Library.js
+++ audiodb/src/component/Library/Library.js
@@ -1,51 +1,65 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios"
 import "../../css/style.css"
+import Albums from '../Albums/albums'
 
 const Library = ({artistName}) => {
     const [albums, setAlbums] = useState([])
+    const [ song, setSongs ] = useState([])
+    const [error, setError] = useState(null)
     //console.log(artistName)
 
     useEffect(() => {
         const fetchAlbums = async () => {
             try {
                 const response = await axios.get("https://www.theaudiodb.com/api/v1/json/123/search.php?s=" + artistName)
-                setAlbums(response.data.artists)
-                console.log(response.data)
+                                
+                if(!response.data.artists){
+                   setError("Artist not found")
+                }
+                setAlbums(response.data.artists) 
+
             }catch (error){
-                console.log(error)
+              setError(error)
             }
+            
         }        
-        fetchAlbums()
+        fetchAlbums()      
     },[])
 
 
   return (
-    <div className='container_library'>
-        
-        <section className='band_container'>            
-            {albums.map((album) => {
-                const { strArtist, 
-                        idArtist, 
-                        intBornYear, 
-                        strArtistLogo, 
-                        strBiographyES, 
-                        strArtistBanner, strStyle } = album
-                return(
-                    <article key={idArtist}>
-                        <div className='container_logo'>
-                            <img className='band_banner' src={strArtistBanner}/>
-                            <img className='band_logo' alt='' src={strArtistLogo}/>
-                        </div>
-                        <h2>{strArtist}</h2 >
-                        <p><b>Date born:</b>   {intBornYear}</p>                                                
-                        <p><b>Style:</b> {strStyle}</p>
-                    </article>
-                )
-            })}
-             
-        </section>
-    </div>
+    <>
+        <div className='container_library'>        
+            <section className='band_container'>            
+                {   error? (
+                    <p>{error}</p>
+                ): (
+                    albums.map((album) => {
+                        const { strArtist, 
+                                idArtist, 
+                                intBornYear, 
+                                strArtistLogo, 
+                                strBiographyES, 
+                                strArtistBanner, strStyle } = album
+                        return(
+                            <article key={idArtist}>
+                                <div className='container_logo'>
+                                    <img className='band_banner' src={strArtistBanner}/>
+                                    <img className='band_logo' alt='' src={strArtistLogo}/>
+                                </div>
+                                <h2>{strArtist}</h2 >
+                                <p><b>Date born:</b>   {intBornYear}</p>                                                
+                                <p><b>Style:</b> {strStyle}</p>
+                            </article>
+                        )
+                    })
+                )             
+                }             
+            </section>        
+        </div>
+        <Albums/>
+    </>
   )
 }
 
